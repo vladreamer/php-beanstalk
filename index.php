9d8edec4b2d3667a6edc9c40f9d88fd3646b50b9
Vlad <vladreamer@gmail.com>
2024-11-14 22:15:10 -0500

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+<TITLE>Testing AWS Beanstalk page</TITLE>
+</head>
+<BODY bgcolor=beige>
+<CENTER>
+<br><br><br><br>
+<font color=green><h1>Hi everybody!</h1></font>
+<img src="devops.png" width=500>
+
+<?php
+echo '<p style="color: gold">
+This is AWS Elastic Beanstalk WebServer with PHP App!!!</p>';
+echo '<br>Server IP:'.$_SERVER['SERVER_ADDR'];
+?>
+<br>
+<font color=blue><h2>TESTING PURPOSES!</h2>
+<font color=red><h2> version #1 </h2>
+</CENTER>
+</BODY>
+</html>
